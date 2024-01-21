85b34fa2d9338f0981895ff82f2541cae3e20893
Aitor Reviriego Amor <aitorevi@gmail.com>
2024-01-21 18:22:05 +0100

    :construction: home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,13 @@ export default {
 				primary: '#212737',
 				secondary: '#ff6b01',
 				tertiary: '#eaedf3',
+				quaternary: '#aeb2bb',
 			},
 			fontFamily: {
 				ibm: ['IBM Plex Mono', 'monospace'],
+			},
+			width: {
+				'116': '29rem',
 			}
 		},
 	},
