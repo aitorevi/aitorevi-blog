ac3ade34216cf5399423ba3e3d95a1b95dd6187f
Aitor Reviriego Amor <aitorevi@gmail.com>
2024-02-07 19:50:35 +0100

    config deploy vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), icon(), react()]
 });
