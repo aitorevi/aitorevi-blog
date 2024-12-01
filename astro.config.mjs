a70a3756c6e1daf37f2821ddcc1a542c7bdf49bf
Aitor Reviriego Amor <aitor.reviriego@leanmind.es>
2024-12-01 12:14:44 +0000

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  integrations: [tailwind(), icon(), react(), sitemap()],
+  site: "https://www.aitorevi.dev/",
 });
